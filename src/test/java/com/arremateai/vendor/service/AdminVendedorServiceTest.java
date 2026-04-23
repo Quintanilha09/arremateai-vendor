53723e2304aa9c7a07b17dd8d921399d4e9c222a
Quintanilha09 <gabriel.hemendinger@gmail.com>
2026-04-23 15:57:16 -0300

    fix(E16-H7): corrige imports duplicados/corrompidos em testes do vendor

--- src/test/java/com/arremateai/vendor/service/AdminVendedorServiceTest.java
+++ src/test/java/com/arremateai/vendor/service/AdminVendedorServiceTest.java
@@ -34,7 +34,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 class AdminVendedorServiceTest {
 
